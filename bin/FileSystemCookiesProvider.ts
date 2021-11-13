f848ad6851edb3ee144261a9df96eb2f1840d1aa
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-13 15:39:23 +0330

    [:star:] implement the cookie provider

---
+++ bin/FileSystemCookiesProvider.ts
@@ -0,0 +1,23 @@
+import { promises as fs } from "fs";
+import type { Cookies, ICookiesProvider } from "./Bot";
+
+export default class FileSystemCookiesProvider implements ICookiesProvider {
+  private _path: string;
+
+  constructor(path = "./cookies.json") {
+    this._path = path;
+  }
+
+  async getCookies(): Promise<Cookies | null> {
+    try {
+      const cookiesBuffer = await fs.readFile(this._path);
+      return JSON.parse(cookiesBuffer.toString()) as Cookies;
+    } catch {
+      return null;
+    }
+  }
+
+  async setCookies(cookies: Cookies): Promise<void> {
+    await fs.writeFile(this._path, JSON.stringify(cookies));
+  }
+}
