a16760a963b4e4fdf48967f17c04f664833f48df
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-14 12:15:56 +0330

    [:star:] add constants

---
+++ bin/constants.ts
@@ -0,0 +1,6 @@
+import path from "path";
+
+export const ROOT_DIR = "figma-backup-root";
+
+export const BACKUP_DIR = path.join(ROOT_DIR, "backups");
+export const COOKIES_PATH = path.join(ROOT_DIR, "cookies.json");
